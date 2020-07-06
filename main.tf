1bd6e9baa9a7b38b64792158686d7906c8a84e6f
Andre Pimentel <49865323+andrefcpimentel2@users.noreply.github.com>
2020-07-06 16:55:48 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -232,6 +232,15 @@ net stop winrm
 sc.exe config winrm start=auto
 net start winrm
 
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) 
+
+choco feature enable -n allowGlobalConfirmation  
+
+choco install curl 
+
+
+
 Stop-Transcript
 
 EOF

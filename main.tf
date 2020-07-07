a6a31b41fefbe7cd07102e981788da1c2b53d08a
Andre Pimentel <49865323+andrefcpimentel2@users.noreply.github.com>
2020-07-07 13:18:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -164,7 +164,9 @@ resource "azurerm_virtual_machine" "windows" {
   location            = var.location
   resource_group_name = azurerm_resource_group.windows.name
   vm_size             = "Standard_B2s"
-
+  identity {
+     type = "SystemAssigned"
+  }
   network_interface_ids         = [azurerm_network_interface.windows-nic.id]
   delete_os_disk_on_termination = "true"
 
